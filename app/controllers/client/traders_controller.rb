7eb330254b0c2c943324d67703a03457897cfd5c
TatsuyaYasui <109195719+aboutyasui@users.noreply.github.com>
2022-10-23 05:05:58 +0000

    [fix]client側のtraderコントローラ記述修正

--- app/controllers/client/traders_controller.rb
+++ app/controllers/client/traders_controller.rb
@@ -2,7 +2,7 @@ class Client::TradersController < ApplicationController
  before_action :authenticate_client! #ユーザがログインしているかどうかを確認し、ログインしていない場合はユーザをログインページにリダイレクトする。
 
   def show
-    @trader = Trader.find(params[:trader_id])
+    @trader = Trader.find(params[:id])
     @posts = Post.where(trader_id: @trader.id).page(params[:page]).per(5)
     #Whereを使うことで特定の業者(URL上に表示されているtrader_id)の投稿データを取得できる
   end
